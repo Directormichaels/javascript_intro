4d8d7536d51d8cbfe0d44a880baee7ceb945f2ea
Directormichaels <uzonwafor@gmail.com>
2022-06-27 07:09:33 +0100

    Javascript intro task

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # javascript_intro
+This is a html file with a javascript in it
+And outputing my Name, Height and Country
+
+Github Pages URL
+https://directormichaels.github.io/javascript_intro/
---
+++ jscript.js
@@ -0,0 +1,14 @@
+let fullname = document.getElementsByClassName("my_Name")
+let height = document.getElementsByClassName("my_Height")
+let country = document.getElementsByClassName("my_Country")
+
+//Screen
+fullname[0].innerText = "Nwafor Oluwasegun"
+height[0].innerHTML = "6 ft 2 inches"
+country[0].innerHTML = "Nigeria"
+
+
+//console
+console.log(fullname[0].innerText);
+console.log(height[0].innerHTML);
+console.log(country[0].innerHTML);
